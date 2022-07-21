74bee8c3a6a13dfe1396cc85dacc3ecb2673eb8e
Nicolas <nicolassoares.santos@outlook.com>
2022-07-21 16:04:00 -0300

    Lógica de codificação criada e aplicada, e correçōes no HTML

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,8 +1,32 @@
-const inputText = document.querySelector('[data-content="userText"]');
+//Extrai o data-content="userText" do HTML e armazena em uma variavel
+const inputText = document.querySelector('[data-content="userText"]')
+const outputText = document.querySelector('[data-content="resultText"]')
 const actionEncrypt = document.querySelector('[data-btn="encryptText"]')
 
-actionEncrypt.addEventListener("click", () => {
-    console.log(inputText.value)
+console.log(actionEncrypt)
+console.log(inputText)
+console.log(outputText)
+
+let codeTxt = ''
+
+actionEncrypt.addEventListener('click', () => {
+
+  if (inputText.value != ''){
+    codeTxt = inputText.value;
+    codeTxt = codeTxt.replace(/e/g, "enter");
+    codeTxt = codeTxt.replace(/i/g, "imes");
+    codeTxt = codeTxt.replace(/a/g, "ai");
+    codeTxt = codeTxt.replace(/o/g, "ober");
+    codeTxt = codeTxt.replace(/u/g, "ufat");
+
+    inputText.value = '';
+    outputText.textContent = '';
+    outputText.textContent = codeTxt;
+
+    console.log(outputText)
+  }
+
+  inputText.textContent = ''
 })
 
 // encryptButton.addEventListener("click", event => {
