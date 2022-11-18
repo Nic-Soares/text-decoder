fb333b50837efb83d0f383536bf3ac368514669c
NicolasSSantos <nicolassoares.santos@outlook.com>
2022-11-18 20:50:42 -0300

    Correções e Descrições para os códigos em JS

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,16 +1,22 @@
 //Textarea de entrada de texto
 const inputText = document.querySelector('[data-content="userText"]');
+
 //Textarea de saida de texto
 const outputText = document.querySelector('[data-content="resultText"]');
+
 //Botao de encrypt
 const actionEncrypt = document.querySelector('[data-btn="encryptText"]');
-//Botao de encrypt
+
+//Botao de Desencriptar
 const actionDecrypt = document.querySelector('[data-btn="decryptText"]');
 
+//Botao de Copiar Texto Encriptado
 const actionCopy = document.querySelector('[data-btn="copyText"]');
 
+// Variavel para resetar textarea
 let codeTxt = ''
 
+//Coleta o conteudo presente no Textarea "userText" e realiza a codificaçao
 actionEncrypt.addEventListener('click', () => {
 
   if (inputText.value !== ''){
@@ -28,6 +34,7 @@ actionEncrypt.addEventListener('click', () => {
   }
 })
 
+//Coleta o conteudo presente no Textarea "resultText" e realiza a descodificacao
 actionDecrypt.addEventListener('click', () => {
 
   if (inputText.value != ''){
@@ -44,35 +51,39 @@ actionDecrypt.addEventListener('click', () => {
   }
 })
 
+//Copia o conteudo do Textarea "resultText" para a area de transferencia
 actionCopy.addEventListener('click', () => {
   navigator.clipboard.writeText(outputText.textContent)
 })
 
-inputText.addEventListener('keypress', function(e) {
+//Identifica qual caracter foi clicada
+inputText.addEventListener('keypress', (e) => {
   if(!checkChar(e)) {
+    console.log(e.preventDefault)
     e.preventDefault();
   }
 });
 
+//Compara se as caracteres clicadas fazem parte da regex
 function checkChar(e) {
   const char = String.fromCharCode(e.keyCode);
 
-  if(char.match('[a-z]')) {
+  if(char.match('[a-z\ ]')) {
     console.log(char);
     return true;
   }
 }
 
-inputText.addEventListener("paste", function() {
+// inputText.addEventListener("paste", function() {
   
-  const regex = new RegExp("^[0-9a-zA-Z\b]+$")
-  const self = this;
+//   const regex = new RegExp("^[0-9a-zA-Z\b]+$")
+//   const self = this;
 
-  setTimeout(function() {
-    const text = self.value;
+//   setTimeout(function() {
+//     const text = self.value;
 
-    if(!regex.test(text)) {
-      self.value = ""
-    }
-  }, 10)
-})
+//     if(!regex.test(text)) {
+//       self.value = ""
+//     }
+//   }, 10)
+// })
